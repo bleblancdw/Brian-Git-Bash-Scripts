1ba8a75b043da2e78b3994eb997a0051b7060775
bleblanc <bleblanc@demandware.com>
2013-03-18 11:27:06 -0400

    Initial Commit

---
+++ custom-ps1.sh
@@ -0,0 +1,25 @@
+export PS1='$(git branch &>/dev/null;\
+        if [ $? -eq 0 ]; then \
+                echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
+                if [ "$?" -eq "0" ]; then \
+                        # @4 - Clean repository - nothing to commit
+                        echo "\n----'$IWhite$Time12h$Color_Off' ""'$IBlue'""\u@\h""'$Green'"$(__git_ps1 " (%s)"); \
+                else \
+                # @5 - Changes to working tree
+                echo "\n----'$IWhite$Time12h$Color_Off' ""'$IBlue'""\u@\h""'$IRed'"$(__git_ps1 " {%s}"); \
+                fi) '$BYellow$PathShort$Color_Off'\n'±' "; \
+        else \
+                  # @2 - Prompt when not in GIT repo
+                  echo "[ '$IWhiteBlack$Time12h$Color_Off' ""'$IBlue'""\u@\h ""'$Yellow$PathShort$Color_Off' ] \$ "; \
+        fi)'
+
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+alias link_home='cd ~/gitHome/LINK_CODE/'
+alias link_arch='cd ~/gitHome/LINK_CODE/LINK_ARCHIVE'
